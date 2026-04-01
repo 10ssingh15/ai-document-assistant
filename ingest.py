3047366f010ee66361858a54f452c2e7712f3cc1
10ssingh15 <61596453+10ssingh15@users.noreply.github.com>
2026-04-01 16:50:38 +0100

    Created function that converts text chunks into embeddings and then stores it into a vector database.

--- ingest.py
+++ ingest.py
@@ -1,7 +1,10 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
+#from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
+from dotenv import load_dotenv
+load_dotenv()
 import os 
 
 def load_documents():
@@ -10,10 +13,21 @@ def load_documents():
     document = load_file.load()
     return document
 
-def chunk_text(document):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+# Chunks text into smaller text for LLMs to handle
+def split_documents(document):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     text_chunks = text_splitter.split_documents(document)
     return text_chunks
 
-docs = chunk_text(load_documents())
-print(len(docs))
+# Text chunks are converted into embeddings (vectors) and stored in a vector db (Chroma).
+def create_vector_store(chunks):
+    embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vector_store = Chroma.from_documents(documents=chunks, embedding=embedding_model, persist_directory = "vector_db")
+    vector_store.persist()
+
+
+
+docs = load_documents()
+chunks = split_documents(docs)
+create_vector_store(chunks)
+#print(len(chunks))
